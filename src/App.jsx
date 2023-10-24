eb56973b7462fb0b14d4bb487433ddfdb657d627
Utkarsh Mishra <79663484+utkarsh7m@users.noreply.github.com>
2023-10-25 00:20:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
